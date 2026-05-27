70dd55e29c855668dd3b114a432b3181034eb86f
KenzieAgustin <kenzieagustin64@gmail.com>
2026-05-27 16:12:29 +0700

    register for testing 1 web routes for view ticket

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\TicketZoneController;
+use App\Http\Controllers\WaitListController;
+use App\Http\Controllers\TicketTokenController;
+use App\Http\Controllers\PricingRuleController;
 
+//masih redirect ke tiket karena belum ada login 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tickets');
 });
+
+Route::get('/view-tickets', [TicketController::class, 'indexWeb']);
+//daftar tiket dan zonanya
+Route::get('/tickets', [TicketController::class, 'index']);
+Route::post('/tickets', [TicketController::class, 'store']);
+
+Route::get('/zones/{ticket_id}', [TicketZoneController::class, 'getZoneByTicket']);
+Route::post('/zones/reduce', [TicketZoneController::class, 'reduceQuota']);
+
+Route::get('/tickets/{id}/calculate-price', [PricingRuleController::class, 'calculateFinalPrice']);
+
+Route::post('/waitlist/join', [WaitListController::class, 'joinWaitList']);
+
+Route::post('/tokens/generate', [TicketTokenController::class, 'generateToken']);
