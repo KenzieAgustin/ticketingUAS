dd4522d916ec4b76b1170784d391c57b73fba4ab
Yohanes-505 <535250054.yohanes@stu.untar.ac.id>
2026-06-04 23:20:56 +0700

    feat: add admin controller to web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => redirect()->route('login'));
