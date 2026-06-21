8a77f97c6d88879e4066cb09bbcdf091fc9a31f4
Yohanes-505 <yohanes.535250054@stu.untar.ac.id>
2026-06-21 20:39:25 +0700

    fix(notifications): rename OrderNotification to AppNotification

--- app/Notifications/AppNotification.php
+++ app/Notifications/AppNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class OrderNotification extends Notification
+class AppNotification extends Notification
 {
     use Queueable;
 
